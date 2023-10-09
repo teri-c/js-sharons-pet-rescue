dc06cc7f3753c4e6fec57f5747dc4ec8c44117a8
teri.c <teri.charles23@gmail.com>
2023-10-09 16:24:41 -0400

    Created factory function, added properties and created two methods

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+const createPet = function (name, species) {
+    const pet = {
+        name: name,
+        species: species,
+        isTired: 5,
+
+        sleep: function () {
+            console.log(`${this.name} needs a nap. Zzz...`);
+        },
+
+        play: function () {
+            if (this.isTired === 10) {
+                console.log("Too tired to play.");
+                this.sleep();
+            } else {
+                console.log(`Yay! ${this.name} loves to play!`);
+                this.isTired += 1;
+            }
+        }
+    };
+    return pet;
+};
